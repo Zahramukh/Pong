334e25f7fec7f6b068a31b0690a9dba3341bd135
Zahramukh <103242033+Zahramukh@users.noreply.github.com>
2022-06-03 11:19:34 +0200

    Update 1spiller.js

---
+++ 1spiller.js
@@ -0,0 +1,54 @@
+let xBall = Math.floor(Math.random() * 300) + 50;
+let yBall = 50;
+let xSpeed = 2;
+let ySpeed = 7;
+let score = 0;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+//Background
+
+function draw() {
+    background(0);
+
+    fill("#ffffff");
+    rect(mouseX, 375, 90, 15);
+
+    move();
+    display();
+    bounce();
+    paddle();
+
+    fill("#d9c3f7");
+    textSize(24);
+    text("Score: " + score, 10, 25);
+}
+
+function move() {
+    xBall += xSpeed;
+    yBall += ySpeed;
+}
+
+function bounce() {
+    if (xBall < 10 || xBall > 400 - 10) {
+        xSpeed *= -1;
+    }
+    if (yBall < 10 || yBall > 400 - 10) {
+        ySpeed *= -1;
+    }
+}
+
+function display() {
+    fill("#d9c3f7");
+    ellipse(xBall, yBall, 20, 20);
+}
+
+function paddle() {
+    if (xBall > mouseX && xBall < mouseX + 90 && yBall + 10 >= 375) {
+        xSpeed *= -1;
+        ySpeed *= -1;
+        score++;
+    }
+}
